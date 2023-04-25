252d1865d5aac20dfe47a2b8e289e386fee8236e
Adam Tai <adamtai@hotmail.co.uk>
2023-04-25 16:53:53 +0100

    data.js updated, function names changed to match JSON data

--- data.js
+++ data.js
@@ -1,26 +1,25 @@
-const examsData = require('./exams.json');
+const examsData = require("./data/TechTestJson.json");
 
 function getAllExams() {
-    return examsData;
-  }
-  function getExamById(id) {
-    return examsData.find((exam) => exam.id === id);
-  }
-  function filterExamsByDate(date) {
-    return examsData.filter((exam) => exam.date === date);
-  }
-  function filterExamsByCandidate(candidate) {
-    return examsData.filter((exam) => exam.candidate === candidate);
-  }
-  function filterExamsByLocation(location) {
-    return examsData.filter((exam) => exam.location === location);
-  }
+  return examsData;
+}
+function getExamById(id) {
+  return examsData.find((exam) => exam.id === id);
+}
+function filterExamsByDate(date) {
+  return examsData.filter((exam) => exam.Date === date);
+}
+function filterExamsByCandidate(candidate) {
+  return examsData.filter((exam) => exam.CandidateName === candidate);
+}
+function filterExamsByLocation(location) {
+  return examsData.filter((exam) => exam.LocationName === location);
+}
 
-  module.exports = {
-    getAllExams,
-    getExamById,
-    filterExamsByDate,
-    filterExamsByCandidate,
-    filterExamsByLocation,
-  };
-  
+module.exports = {
+  getAllExams,
+  getExamById,
+  filterExamsByDate,
+  filterExamsByCandidate,
+  filterExamsByLocation,
+};
