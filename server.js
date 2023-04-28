586284cb47c15b917593b0590eaf066a897afabc
Adam Tai <adamtai@hotmail.co.uk>
2023-04-28 14:39:59 +0100

    CORS added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const data = require("./data/TechTestJson.json");
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
